ea8f37ba37c2017d62a9489662065af6536d8128
Iamneo <techadmin@iamneo.ai>
2023-11-07 11:45:17 +0000

    Backup Commit

--- MovieApp/Model/Detail.cs
+++ MovieApp/Model/Detail.cs
@@ -7,7 +7,7 @@ namespace MovieApp.Models
     public class Detail
     {
         [Key]
-        public int DetailId{get;set;}
+        public int DeatilId{get;set;}
         [Required]
         [MaxLength(30)]
         public string ?Actor{get;set;}
